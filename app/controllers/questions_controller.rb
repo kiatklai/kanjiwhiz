1499b1136ea2a73be0ef6d1bc97a4366e18ba974
kiatklai <likhitthaworn.k@gmail.com>
2023-06-19 22:05:35 +0900

    question views/コントローラーアクション

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,26 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    if Question.create(question_params)
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    question = question.find(params[:id])
+    if question.destroy
+      redirect_to questions_path
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question_text, :correct_answer)
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
-
+  validates :question_text, presence: true
+  validates :correct_answer, presence: true
 end
