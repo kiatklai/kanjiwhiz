00f76fcace9cb8e433e4fe026bf1b1cde15e9085
kiatklai <likhitthaworn.k@gmail.com>
2023-06-20 14:48:08 +0900

    admin権限実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
@@ -8,38 +9,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params)
-      redirect_to user_path(current_user.id)
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(user.id)
     else
       render :edit
     end
   end
 
-  # 管理者専用の登録アクション
-  def new_admin
-    @user = User.new
-  end
-
-  def create_admin
-    @user = User.new(admin_params)
-    @user.admin = true
-
-    if @user.save
-      # 保存が成功した場合の処理
-      redirect_to root_path, notice: '管理者アカウントが作成されました。'
-    else
-      # 保存が失敗した場合の処理
-      render :new_admin
-    end
-  end
-
   private
 
-  def admin_params
-    params.require(:user).permit(:name, :password, :admin)
+  def user_params
+    params.require(:user).permit(:name,:email,:profile_picture, :birthday, :password)
   end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :profile_picture)
-  end
 end
