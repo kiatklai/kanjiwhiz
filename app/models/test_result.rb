392aed85bacbdf50030ccf1133b8fca81ac8c066
kiatklai <likhitthaworn.k@gmail.com>
2023-06-24 13:52:24 +0900

    バリデーション変更

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -2,5 +2,5 @@ class TestResult < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
-  validates :user_answer, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/, message: 'は全角ひらがな・カタカナを使用してください' }
+  validates :user_answer, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: 'は全角ひらがなを使用してください' }
 end
