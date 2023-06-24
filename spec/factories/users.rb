0756ba20dc648f6c4d23bcbf633a38db4d180456
kiatklai <likhitthaworn.k@gmail.com>
2023-06-24 15:34:04 +0900

    userモデル単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    name                  {Faker::Name.initials(number: 2)}
+    birthday              {'1930-01-01'}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 4) }
+    password_confirmation {password}
+    test_attempts         {2}
+    average_score         {5}
+
+    after(:build) do |message|
+      message.profile_picture.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録ができる場合' do
+      it '全ての項目の入力がが存在すれば登録できること' do
+        expect(@user).to be_valid
+      end
+
+      it 'test_attemptsが空でも登録できること' do
+        @user.test_attempts = ''
+        expect(@user).to be_valid
+      end
+
+      it 'average_scoreが空でも登録できること' do
+        @user.average_score = ''
+        expect(@user).to be_valid
+      end
+
+      it 'profile_pictureが空でも登録できること' do
+        @user.profile_picture = ''
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録ができない場合' do
+      it 'nameが空では登録できないこと' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前を入力してください")
+      end
+
+      it 'emailが空では登録できないこと' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("メールを入力してください")
+      end
+
+      it '重複したemailが存在する場合登録できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("メールはすでに存在します")
+      end
+
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('メールは不正な値です')
+      end
+
+      it 'birthdayが空では登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("生年月日を入力してください")
+      end
+
+      it 'passwordが空では登録できないこと' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+      end
+
+      it 'passwordが5文字以下であれば登録できないこと' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
+      end
+
+      it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは128文字以内で入力してください")
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できないこと' do
+        @user.password = '12345a'
+        @user.password_confirmation = '123456a'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      end
+    end
+  end
 end
