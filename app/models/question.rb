0768d87e53e5b00dd1d06c79751191d2ab24d038
kiatklai <likhitthaworn.k@gmail.com>
2023-06-24 20:15:12 +0900

    dependent: :destroy オプションを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  has_many :test_results
+  has_many :test_results, dependent: :destroy
 
   validates :question_text, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな・カタカナ・漢字を使用してください' }
   validates :correct_answer, presence: true,format: { with: /\A[ぁ-んー－]+\z/, message: 'は全角ひらがなを使用してください' }
